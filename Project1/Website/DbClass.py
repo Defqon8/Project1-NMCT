61826807ada6c16d23e6f2c5c1faa2b6ca212b2c
Defqon8 <bram.robyn@student.howest.be>
2017-06-12 16:22:54 +0200

    8/6/2017

--- Project1/Website/DbClass.py
+++ Project1/Website/DbClass.py
@@ -54,33 +54,33 @@ class DbClass:
 
 
 
-    def getDataFromDatabase(self):
-        # Query zonder parameters
-        sqlQuery = "SELECT * FROM tabelnaam"
-
-        self.__cursor.execute(sqlQuery)
-        result = self.__cursor.fetchall()
-        self.__cursor.close()
-        return result
-
-
-    # def getDataFromDatabaseMetVoorwaarde(self, voorwaarde):
-    #     # Query met parameters
-    #     sqlQuery = "SELECT * FROM tablename WHERE columnname = '{param1}'"
-    #     # Combineren van de query en parameter
-    #     sqlCommand = sqlQuery.format(param1=voorwaarde)
+    # def getDataFromDatabase(self):
+    #     # Query zonder parameters
+    #     sqlQuery = "SELECT * FROM tabelnaam"
     #
-    #     self.__cursor.execute(sqlCommand)
+    #     self.__cursor.execute(sqlQuery)
     #     result = self.__cursor.fetchall()
     #     self.__cursor.close()
     #     return result
-
-    def setDataToDatabase(self, value1):
-        # Query met parameters
-        sqlQuery = "INSERT INTO tablename (columnname) VALUES ('{param1}')"
-        # Combineren van de query en parameter
-        sqlCommand = sqlQuery.format(param1=value1)
-
-        self.__cursor.execute(sqlCommand)
-        self.__connection.commit()
-        self.__cursor.close()
+    #
+    #
+    # # def getDataFromDatabaseMetVoorwaarde(self, voorwaarde):
+    # #     # Query met parameters
+    # #     sqlQuery = "SELECT * FROM tablename WHERE columnname = '{param1}'"
+    # #     # Combineren van de query en parameter
+    # #     sqlCommand = sqlQuery.format(param1=voorwaarde)
+    # #
+    # #     self.__cursor.execute(sqlCommand)
+    # #     result = self.__cursor.fetchall()
+    # #     self.__cursor.close()
+    # #     return result
+    #
+    # def setDataToDatabase(self, value1):
+    #     # Query met parameters
+    #     sqlQuery = "INSERT INTO tablename (columnname) VALUES ('{param1}')"
+    #     # Combineren van de query en parameter
+    #     sqlCommand = sqlQuery.format(param1=value1)
+    #
+    #     self.__cursor.execute(sqlCommand)
+    #     self.__connection.commit()
+    #     self.__cursor.close()
--- Project1/Website/ProjectFlask.py
+++ Project1/Website/ProjectFlask.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask import render_template
+from DbClass import DbClass
 import os
 
+
 app = Flask(__name__)
-
-
 
 @app.route('/')
 def homepage():
