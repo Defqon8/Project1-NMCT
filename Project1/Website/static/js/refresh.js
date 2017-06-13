709e32142cdcf1e69eec5dd6c118bd618eb3a317
Defqon8 <bram.robyn@student.howest.be>
2017-06-13 12:08:51 +0200

    automatic cash clear

---
+++ Project1/Website/static/js/refresh.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Bram Robyn on 13/06/2017.
+ */
+// Check if a new cache is available on page load.
+window.addEventListener('load', function(e) {
+
+  window.applicationCache.addEventListener('updateready', function(e) {
+    if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+      // Browser downloaded a new app cache.
+      // Swap it in and reload the page to get the new hotness.
+      window.applicationCache.swapCache();
+      if (confirm('A new version of this site is available. Load it?')) {
+        window.location.reload();
+      }
+    } else {
+      // Manifest didn't changed. Nothing new to server.
+    }
+  }, false);
+
+}, false);
